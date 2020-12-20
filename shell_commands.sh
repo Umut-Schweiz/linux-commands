9df0a74ae0cf30474f04393584463f0ae84d37e0
Umut <av.umutgungor92@gmail.com>
2020-12-20 18:34:46 +0100

    basic shell commands

---
+++ shell_commands.sh
@@ -0,0 +1,38 @@
+Linux terminalinde shell komutlari ile asagidaki islemleri yapiniz.
+
+1. Hangi klasorde (directory) oldugunuzu kontrol edin. ($ ...)
+2. Gizli dosyalar dahil tum icerigi listeleyin.
+3. Yeni bir klasor olusturun.
+4. Yeni bir dosya (file) olusturun. 
+5. Dosyayi klasorun icine kopyalayin.
+6. Bulundugunuz yerden klasorun icine gecin. 
+7. Kopyalaginiz dosyayi silin.
+8. Icinde "Birinci dosya temel shell komutlari" yazan bir dosya olusturun.
+9. Dosya icerigini goruntuleyin.
+10. Dosyada "shell" kelimesi gecip gecmedigini filtreleyerek kontrol edin.
+11. Icinde "Ikinci dosya disk islemleri" yazan yeni bir dosya olusturun.
+12. Iki dosyanin icerigini yeni bir dosyada birlestirin.
+13. Bir ust klasore geciniz (Daha onceki konuma).
+14. Klasoru silin (Icinde dosyalar oldugu halde).
+15. root hesabına gecin.
+
+Github'da "linux-commands" repositorysi olusturun, kullandiginiz komutlari "shell_commands.sh" dosyasina kaydedip gonderin. 
+
+********************************************************
+1. pwd 
+2. ls -all
+3. mkdir Folder
+4. touch file
+5. cp file Folder
+6. cd Folder
+7. rm file
+8. echo "Birinci dosya temel shell komutlari" >> newFile
+9. cat newFile
+10. grep shell newFile
+11. echo "Ikinci dosya disk islemleri" >> newFile2
+12. cat newFile1 newFile2 >> newConcatFile
+13. cd ..
+14. rm -r Folder
+15. sudo root
+
+**********************************************************
